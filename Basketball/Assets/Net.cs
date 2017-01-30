b8c1051ba75f34ef1a8cf50a10c82c82288f51e1
Gabe <bub.the.red@gmail.com>
2017-01-30 10:57:02 -0500

    Added basketball game to repo
    I did it.

---
+++ Basketball/Assets/Net.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Net : MonoBehaviour {
+	public ParticleSystem partSys;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		partSys.Play ();
+
+	}
+		
+}
---
+++ Basketball/Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	bool jumping;
+	float curJumpVel;
+	float startY;
+
+	public float jumpSpd;
+	public float gravity;
+	public Rigidbody2D ballRB;
+	public Vector2 flyForce;
+	bool thrown; 
+
+	// Use this for initialization
+	void Start () {
+		startY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DealWithInput ();
+		if (jumping) {
+			DealWithJumping ();
+		}
+	}
+
+	void DealWithInput () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (jumping) {
+				ThrowBall();
+			} else {
+				Jump();
+			}
+		}
+	}
+
+	void Jump () {
+		jumping = true;
+		curJumpVel = jumpSpd;
+	}
+
+	void ThrowBall () {
+		if (!thrown) {
+			ballRB.isKinematic = false;
+			ballRB.transform.parent = null;
+			ballRB.AddForce (flyForce, ForceMode2D.Impulse);
+			thrown = true;
+		}
+	}
+
+
+	void DealWithJumping() {
+		transform.position += new Vector3 (0, curJumpVel, 0);
+		curJumpVel -= gravity;
+		if (transform.position.y <= startY) {
+			jumping = false;
+			transform.position = new Vector3 (transform.position.x, startY, 0);
+		}
+	}
+}
